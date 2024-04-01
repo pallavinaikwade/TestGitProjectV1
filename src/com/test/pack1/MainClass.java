0da1a5aeb1a24e0acfa7a7b7ad145c722921547b
pnaikwade <pnaikwade@L14-PNAIKWADE.ind1.echo.quinstreet.net>
2024-04-01 18:08:03 +0530

    First employee commit

---
+++ src/com/test/pack1/MainClass.java
@@ -0,0 +1,14 @@
+package com.test.pack1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Employee emp1 = new Employee(101, "Pallavi");
+		Employee emp2 = new Employee(102, "Vishu");
+		
+		System.out.println("emp1: "+ emp1);		
+		System.out.println("emp2: "+ emp2);
+
+	}
+
+}
