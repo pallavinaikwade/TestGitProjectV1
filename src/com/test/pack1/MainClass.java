f4186c66115c3116f34b9e71d5487489e3c6dac2
vishwanath <vishunaikwade1@gmail.com>
2024-04-01 18:42:55 +0530

    i added Vishwanath

--- src/com/test/pack1/MainClass.java
+++ src/com/test/pack1/MainClass.java
@@ -5,9 +5,11 @@ public class MainClass {
 	public static void main(String[] args) {
 		Employee emp1 = new Employee(101, "Pallavi");
 		Employee emp2 = new Employee(102, "Vishu");
+		Employee emp3 = new Employee(103, "Vishwanath");
 		
 		System.out.println("emp1: "+ emp1);		
 		System.out.println("emp2: "+ emp2);
+		System.out.println("emp3: "+ emp3);
 
 	}
 
